e06513598bf35f97c0b6baae94efb936a43fb4fe
Jonathan Roth <rothje@vcu.edu>
2019-11-05 21:21:05 -0500

    added README

---
+++ app/data/friends.js
@@ -0,0 +1,103 @@
+
+var friends = [
+  {
+    name: "Mike",
+    photo: "https://bloximages.newyork1.vip.townnews.com/winchesterstar.com/content/tncms/assets/v3/editorial/0/9e/09ed03da-2d14-5fea-96b8-d916265ba2b2/5d1faada7bc87.image.jpg?resize=314%2C400",
+    scores: [
+      "2",
+      "3",
+      "4",
+      "4",
+      "1",
+      "1",
+      "3",
+      "5",
+      "1",
+      "4"
+    ]
+  },
+  {
+    name: "Agatha",
+    photo: "https://vignette.wikia.nocookie.net/dark-deception-game/images/2/26/Agatha_Cycles.png/revision/latest/scale-to-width-down/1000?cb=20190831230719",
+    scores: [
+      "4",
+      "4",
+      "3",
+      "2",
+      "1",
+      "3",
+      "4",
+      "1",
+      "3",
+      "2"
+    ]
+  },
+  {
+    name: "Dink Scattlebrown",
+    photo: "https://images-na.ssl-images-amazon.com/images/I/81yfxCWU3JL._UL1500_.jpg",
+    scores: [
+      "5",
+      "2",
+      "2",
+      "2",
+      "4",
+      "1",
+      "3",
+      "2",
+      "5",
+      "5"
+    ]
+  },
+  {
+    name: "Wang",
+    photo: "https://i.kym-cdn.com/entries/icons/facebook/000/023/419/nerd_smoking.jpg",
+    scores: [
+      "5",
+      "3",
+      "5",
+      "5",
+      "3",
+      "1",
+      "4",
+      "3",
+      "1",
+      "3"
+    ]
+  },
+  {
+    name: "Chrissy",
+    photo: "https://i.ytimg.com/vi/mpJ97jO-fCg/maxresdefault.jpg",
+    scores: [
+      "4",
+      "3",
+      "4",
+      "5",
+      "5",
+      "4",
+      "4",
+      "2",
+      "1",
+      "5"
+    ]
+  },
+  {
+    name: "Bo",
+    photo: "https://i.ytimg.com/vi/hSmWPBexgV0/hqdefault.jpg",
+    scores: [
+      "4",
+      "2",
+      "2",
+      "2",
+      "2",
+      "2",
+      "3",
+      "2",
+      "2",
+      "5"
+    ]
+  }
+
+];
+
+// Here we export the array. This makes it accessible to other files using require.
+module.exports = friends;
